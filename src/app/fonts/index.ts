0d7279c074d5ff476b7e1b9c1b9ea52c12ad24a4
Ting <ting.yun.career@gmail.com>
2024-03-01 19:39:11 -0800

    open sans

---
+++ src/app/fonts/index.ts
@@ -0,0 +1,8 @@
+import { Inter, Open_Sans } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const OpenSans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+});
